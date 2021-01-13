2f6cb6700c9ac8c1fc1fcf50af50994c472dd743
Erjon <erjonlusha@gmail.com>
2021-01-13 16:35:45 +0100

    Voto mancante

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*  vue-boolflix
 Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
-// così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, 
+Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
+così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, 
 lasciando le restanti vuote (troviamo le icone in FontAwesome).
 Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezzepiene (o mezze vuote :P)
 
@@ -43,7 +43,7 @@ let app = new Vue ({
         vote_average:[],
         searchMoviesAPi:'',
         searchTvAPi:'',
-        search:'ci',
+        search:'',
         movies: [],
         seriesTv: [],
         imgNonDisponibile:'https://tabook.it/img/cover_non_disp.jpg'
